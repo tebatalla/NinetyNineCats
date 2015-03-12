a89a6f5d2507d238c4d491d86826d2a5b2188d2c
Tomas Batalla <tebatalla@gmail.com>
2015-03-11 18:00:04 -0700

    fixed approval/denial

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -33,29 +33,28 @@ class CatRentalRequest < ActiveRecord::Base
     self.save!
   end
 
-  private
-    def overlapping_pending_requests
-      overlapping_requests.select { |request| request.pending? }
-    end
+  def overlapping_pending_requests
+    overlapping_requests.select { |request| request.pending? }
+  end
 
-    def overlapping_requests
-      CatRentalRequest.find_by_sql([<<-SQL, id: id, cat_id: cat_id, start_date: start_date, end_date: end_date])
-        SELECT
-          *
-        FROM
-          cat_rental_requests
-        WHERE
-          (cat_rental_requests.id <> :id OR :id IS NULL) AND
-          cat_id = :cat_id AND
-          (
-            :start_date <= end_date AND start_date <= :end_date
-          )
-      SQL
-    end
+  def overlapping_requests
+    CatRentalRequest.find_by_sql([<<-SQL, id: id, cat_id: cat_id, start_date: start_date, end_date: end_date])
+      SELECT
+        *
+      FROM
+        cat_rental_requests
+      WHERE
+        (cat_rental_requests.id <> :id OR :id IS NULL) AND
+        cat_id = :cat_id AND
+        (
+          :start_date <= end_date AND start_date <= :end_date
+        )
+    SQL
+  end
 
-    def overlapping_approved_requests
-      if overlapping_requests.any? { |request| request.status == 'APPROVED' } && status == 'APPROVED'
-        errors.add(:status, "Cannot overlap a request that has been approved")
-      end
+  def overlapping_approved_requests
+    if overlapping_requests.any? { |request| request.status == 'APPROVED' } && status == 'APPROVED'
+      errors.add(:status, "Cannot overlap a request that has been approved")
     end
+  end
 end
